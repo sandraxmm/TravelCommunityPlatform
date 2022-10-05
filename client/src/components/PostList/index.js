736fa1c4232960f59a91db28e2a7412d0867f62d
sandra <sandraxmendoza@gmail.com>
2022-10-04 18:31:11 -0700

    post list component

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -13,11 +13,30 @@ const PostList = ({
         <div>
             {showLocation && <h3>{postlocation}</h3>}
             {posts &&
-            posts.map((post) => (
+                posts.map((post) => (
                 <div key={thought._id} className=''>
-                    
-            )}
+                    <h4 className=''>
+                        {showUsername ? (
+                            <Link className='' to={`/profiles/${post.postAuthor}`}>
+                                {post.postAuthor} <br />
+                                <span style={{fontSize: '1rem' }}>
+                                    posted this on {post.createdAt}
+                                </span>
+                            </Link>
+                        ) : (
+                            <>
+                                <span style={{ fontSize: '1rem' }}>
+                                    You posted this on {post.createdAt}
+                                </span>
+                            </>
+                        )}
+                    </h4>
+                    <div className=''>
+                        <p>{post.postText}</p>
+                    </div>
+                    <Link className='' to={`/posts/${post._id}`}></Link>
+                </div>    
+            ))}
         </div>
-
-    )
-}
+    );
+};
